d395c528a321f3361a51527e9ca73f5e0b5d9b70
Darsh99 <ajudiya.darshan007@gmail.com>
2020-01-02 12:28:07 +0530

    sql edit 2

--- app/src/main/java/com/example/projectapp/DatabseHelper.java
+++ app/src/main/java/com/example/projectapp/DatabseHelper.java
@@ -11,7 +11,7 @@ public class DatabseHelper extends SQLiteOpenHelper {
             public static final String playlist="playlist";
             public static final String slides="slide";
             public static final String component="component";
-
+            public static final String animations="animations";
 
 
     public DatabseHelper(@Nullable Context context) {
@@ -20,8 +20,16 @@ public class DatabseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table "+playlist+"(pid integer primary key ,name text,height  integer, width integer)");
-        //db.execSQL("create table "+slides+"(sid integer  )");
+        //playlist table
+        db.execSQL("create table "+playlist+"(pid integer primary key ,name text,height  real, width real)");
+        //slides table
+        db.execSQL("create table "+slides+"(sid integer,pid integer,name text,bgcolor text,bgimage integer,duration integer,next integer,animate integer,animation integer,audio integer)");
+        //component table
+        db.execSQL("create table "+component+"(cid integer,sid integer,type text,left_pos integer,right_pos integer,top_pos integer,bottom_pos integer,width real,height real,uri text,shadow integer,scalex integer,scaley integer,z_index integer,angle integer,onclick text,uri text,html text,animate integer,enteranim integer,exitanim integer)");
+        //animation table
+        db.execSQL("create table "+animations+"(animid integer,type text,duration integer,delay integer)");
+        //shadow table
+        db.execSQL("");
     }
 
     @Override

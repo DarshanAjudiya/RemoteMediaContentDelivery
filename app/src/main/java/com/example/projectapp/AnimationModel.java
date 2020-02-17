e361d51f2f3d3e5d32485e3141b082a814ea2da4
darshan <ajudiya.darshan007@gmail.com>
2020-02-17 15:24:47 +0530

    custom animation created

--- app/src/main/java/com/example/projectapp/AnimationModel.java
+++ app/src/main/java/com/example/projectapp/AnimationModel.java
@@ -49,18 +49,46 @@ public class AnimationModel {
 
     public Animation getAnimation(Context context) {
         Animation animation = null;
-        animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_left);
         switch (type) {
             case "slide-in":
-
-                break;
-
+            case "slide-in-left":   animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_left);
+                                    break;
+            case "slide-in-right":animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_right);
+                                    break;
+            case "slide-in-top":animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_top);
+                                    break;
+            case "slide-in-bottom":animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_bottom);
+                                    break;
+            case "slide-in-left-rotate":animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_left);
+                                    break;
+            case "slide-in-top-left":animation = AnimationUtils.loadAnimation(context,R.anim.slide_in_top_left);
+                                    break;
+            case "zoom-in":animation = AnimationUtils.loadAnimation(context,R.anim.zoom_in);
+                                    break;
+            case "zoom-in-left":animation = AnimationUtils.loadAnimation(context,R.anim.zoom_in_left);
+                                    break;
+            case "slide-out":
+            case "slide-out-right":animation = AnimationUtils.loadAnimation(context,R.anim.slide_out_right);
+                                    break;
+            case "slide-out-left":animation = AnimationUtils.loadAnimation(context,R.anim.slide_out_left);
+                                    break;
+            case "slide-out-top":animation = AnimationUtils.loadAnimation(context,R.anim.slide_out_top);
+                                    break;
+            case "slide-out-bottom":animation = AnimationUtils.loadAnimation(context,R.anim.slide_out_bottom);
+                                    break;
+            case "slide-out-top-right":animation = AnimationUtils.loadAnimation(context,R.anim.slide_out_top_right);
+                                    break;
+            case "zoom-out":animation = AnimationUtils.loadAnimation(context,R.anim.zoom_out);
+                                    break;
+            case "zoom-out-right":animation = AnimationUtils.loadAnimation(context,R.anim.zoom_out_right);
+                                    break;
         }
-
-        if (duration != null)
-            animation.setDuration(duration*1000);
-        if (delay != null)
-            animation.setStartOffset(delay * 1000);
+        if (animation!=null) {
+            if (duration != null)
+                animation.setDuration(duration * 1000);
+            if (delay != null)
+                animation.setStartOffset(delay * 1000);
+        }
 
 
         return animation;
